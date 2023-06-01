a2d584ea57904ba63bfc21a6ceb11de3cb3db4e9
Felipe Augusto Souza Guimarães <felipe.guimaraes@citsamazonas.local>
2023-06-01 15:38:27 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import { Button } from "./components/Button";
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
+
+function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Button />
+      <Button />
+      <GlobalStyle />
+    </ThemeProvider>
+  );
+}
+
+export default App;
